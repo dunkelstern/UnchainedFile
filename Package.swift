75bbddee13b6a2b1e538c2103915eeeace3b3f59
Johannes Schriewer <hallo@dunkelstern.de>
2015-12-20 23:09:59 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  UnchainedFile
+//
+//  Created by Johannes Schriewer on 2015-12-20.
+//  Copyright © 2015 Johannes Schriewer. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "UnchainedFile"
+)
